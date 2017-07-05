d8f4035e80fa4e76c10047aa6f8c6a77a2660ad3
cooltolia <cooltolia@gmail.com>
2017-07-05 14:07:43 +0300

    Обновил папку docs

--- docs/js/main.js
+++ docs/js/main.js
@@ -73,6 +73,46 @@
     });
 
     
+        $('.best-offer__slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        /*autoplay: true,
+        autoplaySpeed: 3000,*/
+         
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              infinite: true,
+              arrows: true,
+              dots: false,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              centerMode: true,
+              autoplaySpeed: 5000,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true,
+              arrows: false,
+            }
+          }
+        ] 
+    });
         
         
         
@@ -80,8 +120,18 @@
         
         
         
-        
-        
+        $(".carousel").swipe({
+    
+      swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+    
+        if (direction == 'left') $(this).carousel('next');
+        if (direction == 'right') $(this).carousel('prev');
+    
+      },
+      allowPageScroll:"vertical"
+    
+    });
+    
         var hamburger = $(".hamburger");
     var secondaryNav = $(".secondary-nav");
     
