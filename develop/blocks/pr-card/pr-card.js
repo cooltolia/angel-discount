5ee36ae7558dc55c6e460f66214d9adb7806d329
cooltolia <cooltolia@gmail.com>
2017-07-11 10:25:09 +0300

    Небольшие исправления

---
+++ develop/blocks/pr-card/pr-card.js
@@ -0,0 +1,11 @@
+/*
+var prBody = $(".pr-card__body");
+var discount = $(".pr-card__discount");
+
+for (var i = 0; i < prBody.length; i++) {
+  if (prBody[i].clientWidth <= "200") {
+    discount[i].style.padding = "6px 8px"
+    console.log(prBody[i].clientWidth);
+  }
+}
+*/
